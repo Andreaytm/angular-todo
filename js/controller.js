933266a0f9c7dc9316abb45a0500ffa73d707748
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-02-19 15:19:59 +0000

    added account angular form

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,15 @@
 angular.module("RouteControllers", [])
 	.controller("HomeController", function($scope) {
-		$scope.title ="Welcome to Angular Todo!";
+		$scope.title ="Welcome to Angular Todo!"
+	})
+	.controller("RegisterController", function($scope){
+		$scope.registrationUser = {};
+
+		$scope.submitForm = function(){
+			if($scope.registrationForm.$valid) {
+				$scope.registrationUser.username = $scope.user.username;
+				$scope.registrationUser.password = $scope.user.password;
+			}
+			console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
+		};
 	});
