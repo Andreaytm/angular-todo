baf17a362fa13119b8668385190d1c9dd436d56e
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-02-20 22:36:43 +0000

    Updated login authentication

--- js/controller.js
+++ js/controller.js
@@ -2,21 +2,31 @@ angular.module("RouteControllers", [])
 	.controller("HomeController", function($scope) {
 		$scope.title ="Welcome to Angular Todo!";
 	})
-	.controller("RegisterController", function($scope, $location, UserAPIService){
+	.controller("RegisterController", function($scope, UserAPIService) {
 		$scope.registrationUser = {};
 		var URL="https://morning-castle-91468.herokuapp.com/";
 
-		$scope.submitForm = function(){
+		$scope.login = function() {
+			UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
+				$scope.token = results.data.token;
+				console.log($scope.token);
+			}).catch(function(err) {
+				console.log(err);
+			});
+		}
+
+		$scope.submitForm = function() {
 			if($scope.registrationForm.$valid) {
 				$scope.registrationUser.username = $scope.user.username;
 				$scope.registrationUser.password = $scope.user.password;
 
-				UserAPIService.registerUser(URL +"accounts/register/", $scope.registrationUser).then(function(results){
+				UserAPIService.callAPI(URL +"accounts/register/", $scope.registrationUser).then(function(results) {
 					$scope.data = results.data;
 					alert("You have successfully registered to Angular Todo");
-				}).catch(function(err){
-					alert("Oops! Something went wrong!");
+					$scope.login();
+				}).catch(function(err) {
 					console.log(err);
+					alert("Registration failed, please try again with another username.");
 				});
 			}
 		};
