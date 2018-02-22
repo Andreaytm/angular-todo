c7888283425b81c0333ea59e3acee5a086691cc1
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-02-22 22:30:07 +0000

    token to local storage

--- js/controller.js
+++ js/controller.js
@@ -2,16 +2,23 @@ angular.module("RouteControllers", [])
 	.controller("HomeController", function($scope) {
 		$scope.title ="Welcome to Angular Todo!";
 	})
-	.controller("RegisterController", function($scope, UserAPIService) {
+	.controller("RegisterController", function($scope, UserAPIService, store) {
 		$scope.registrationUser = {};
 		var URL="https://morning-castle-91468.herokuapp.com/";
+
+		var authStorage ={
+			name: "StorageTest"
+		};
+
+		store.set("obj",authStorage);
 
 		$scope.login = function() {
 			UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
 				$scope.token = results.data.token;
-				console.log($scope.token);
+				store.set("username", $scope.registrationUser.username);
+				store.set("authToken", $scope.token);
 			}).catch(function(err) {
-				console.log(err);
+				console.log(err.data);
 			});
 		}
 
