49ed7904eb0c12d320c529234e74fc3887b1ee29
Andreaytm <andrea.ma@hotmail.co.uk>
2018-04-06 22:46:13 +0100

    updated code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 angular.module("TodoApp", ["ngRoute", "angular-storage", "RouteControllers", "UserService", "TodoService", "TodoDirective"]);
 
 angular.module("TodoApp").config(function($locationProvider, $routeProvider){
-	$locationProvider.html5Mode(true); //Enable href routing without hashes
 
 	$routeProvider.when("/", {
 		templateUrl: "templates/home.html",
@@ -27,4 +26,6 @@ angular.module("TodoApp").config(function($locationProvider, $routeProvider){
 		templateUrl: "templates/edit-todo.html",
 		controller: "EditTodoController"
 	});
+		$locationProvider.html5Mode(true); //Enable href routing without hashes
+
 });
